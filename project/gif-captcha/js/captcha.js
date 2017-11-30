1a123cf375b9c7352ea16bb800b72c5e0ba9c582
Sigmund Chow <Sigmund.Chow@hsc.utah.edu>
2017-11-29 19:54:09 -0700

    First commit

---
+++ project/gif-captcha/js/captcha.js
@@ -0,0 +1,41 @@
+(function () {
+
+    function gifCaptcha() {
+        this.createGIF = function (images) {
+            gifshot.createGIF({
+                gifWidth: 200,
+                gifHeight: 200,
+                images: images,
+                interval: 0.6,
+                numFrames: 20,
+                frameDuration: 1,
+                fontWeight: 'normal',
+                fontSize: '16px',
+                fontFamily: 'sans-serif',
+                fontColor: '#ffffff',
+                textAlign: 'center',
+                textBaseline: 'bottom',
+                sampleInterval: 10,
+                numWorkers: 2
+            }, function (obj) {
+                if (!obj.error) {
+                    var image = obj.image, animatedImage = document.createElement('img');
+                    animatedImage.src = image;
+                    document.getElementById('captcha-block').appendChild(animatedImage);
+                }
+            });
+        };
+    }
+
+    var main = new gifCaptcha();
+    var images = [
+        'img/tree/1.jpg',
+        'img/random/1.jpg',
+        'img/tree/9.jpg',
+        'img/tree/10.jpg',
+        'img/random/3.jpg'
+    ];
+
+    main.createGIF(images);
+
+})();
