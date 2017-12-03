0509320fc42abfac00ca46f46dceb47a51836382
Sigmund <sigmundccs@gmail.com>
2017-12-02 18:43:37 -0700

    Pivot from gif to grid of 9 images. Add overlay on hover. WIP

--- project/gif-captcha/js/captcha.js
+++ project/gif-captcha/js/captcha.js
@@ -1,46 +1,53 @@
 (function () {
 
-    function gifCaptcha() {
-        this.createGIF = function (images) {
-            gifshot.createGIF({
-                gifWidth: 200,
-                gifHeight: 200,
-                images: images,
-                interval: 0.6,
-                numFrames: 20,
-                frameDuration: 1,
-                fontWeight: 'normal',
-                fontSize: '16px',
-                fontFamily: 'sans-serif',
-                fontColor: '#ffffff',
-                textAlign: 'center',
-                textBaseline: 'bottom',
-                sampleInterval: 10,
-                numWorkers: 2
-            }, function (obj) {
-                if (!obj.error) {
-                    var image = obj.image, animatedImage = document.createElement('img');
-                    animatedImage.src = image;
-                    document.getElementById('captcha-block').appendChild(animatedImage);
+    // function gifCaptcha() {
+    //     this.createGIF = function (images) {
+    //         gifshot.createGIF({
+    //             gifWidth: 200,
+    //             gifHeight: 200,
+    //             images: images,
+    //             interval: 0.6,
+    //             numFrames: 20,
+    //             frameDuration: 1,
+    //             fontWeight: 'normal',
+    //             fontSize: '16px',
+    //             fontFamily: 'sans-serif',
+    //             fontColor: '#ffffff',
+    //             textAlign: 'center',
+    //             textBaseline: 'bottom',
+    //             sampleInterval: 10,
+    //             numWorkers: 2
+    //         }, function (obj) {
+    //             if (!obj.error) {
+    //                 var image = obj.image, animatedImage = document.createElement('img');
+    //                 animatedImage.src = image;
+    //                 document.getElementById('captcha-block').appendChild(animatedImage);
 
-                    // Add audio player
-                    var audio = document.createElement('audio');
-                    audio.id = 'captcha-audio';
-                    document.getElementById('captcha-block').appendChild(audio);
-                }
-            });
+    //                 // Add audio player - audio challenge
+    //                 var audio = document.createElement('audio');
+    //                 audio.id = 'captcha-audio';
+    //                 document.getElementById('captcha-block').appendChild(audio);
+    //             }
+    //         });
+    //     };
+    // }
+
+    // var main = new gifCaptcha();
+    // var images = [
+    //     'img/tree/1.jpg',
+    //     'img/random/1.jpg',
+    //     'img/tree/9.jpg',
+    //     'img/tree/10.jpg',
+    //     'img/random/3.jpg'
+    // ];
+
+    // main.createGIF(images);
+
+    function iCaptcha() {
+        this.createCaptcha = function (images) {
+            
+            var image = document.createElement('img');
         };
     }
-
-    var main = new gifCaptcha();
-    var images = [
-        'img/tree/1.jpg',
-        'img/random/1.jpg',
-        'img/tree/9.jpg',
-        'img/tree/10.jpg',
-        'img/random/3.jpg'
-    ];
-
-    main.createGIF(images);
 
 })();
